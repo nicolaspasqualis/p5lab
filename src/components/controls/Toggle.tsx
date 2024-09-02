648284cd439194e5475feb0fdfec055c4381598b
nicolaspasqualis <32816813+nicolaspasqualis@users.noreply.github.com>
2024-09-02 18:19:27 -0300

    fix styling on checkbox/toggle

--- src/components/controls/Toggle.tsx
+++ src/components/controls/Toggle.tsx
@@ -13,13 +13,13 @@ export const Toggle = ({ label, value, onChange, showValue=true }: ToggleProps) 
       {label}{showValue && <>: <span className="text-xs text-right text-gray-500 font-mono">{value.toString()}</span></>}
     </span>
     <div className='h-full pt-[2px] pb-[2px] flex flex-col justify-center'>
-      <Switch.Root className="flex-grow-0 m-0 w-[22px] h-[12px] p-0 bg-gray-200 rounded-full relative focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
+      <Switch.Root className="focus:outline-none focus:border-blue-500 flex-grow-0 m-0 w-[20px] h-[12px] p-0 bg-white border border-gray-300 rounded-full relative data-[state=checked]:bg-white outline-none cursor-default"
         checked={value}
         onCheckedChange={(val) => {
           onChange(val);
         }}
       >
-        <Switch.Thumb className="block w-[10px] h-[10px] -translate-y-[0] bg-white rounded-full transition-transform duration-100 -translate-x-[0] will-change-transform data-[state=checked]:translate-x-[10px]" />
+        <Switch.Thumb className="block w-[8px] h-[8px] -translate-y-[0] translate-x-[1px] bg-white border border-grey-300 rounded-full transition-transform duration-100 -translate-x-[0] will-change-transform data-[state=checked]:translate-x-[9px] data-[state=checked]:border-black data-[state=checked]:bg-white" />
       </Switch.Root>
     </div>
   </label>
